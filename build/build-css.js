49f98c968c96a63aa0bdb809f7f4a00d95361b8d
XhmikosR <xhmikosr@gmail.com>
2020-11-11 10:12:07 +0200

    Updates
    TODO:
    
    * handle errors
    * switch to the promises fs API
    * update clean-css options
    * don't run postcss for the minified files
    * use the unminified files as clean-css source
    * make sure the map files still work
    * use __dirname so that the script works from root and the build dir

--- build/build-css.js
+++ build/build-css.js
@@ -1,36 +1,45 @@
-const postcssConfig = require('./postcss.config')
-const postcss = require('postcss')
-const glob = require('glob')
-const sass = require('node-sass')
-const cleancss = require('clean-css')
+'use strict'
+
 const fs = require('fs')
 const path = require('path')
-const files = ['bootstrap', 'bootstrap-grid', 'bootstrap-reboot', 'bootstrap-utilities']
+const postcss = require('postcss')
+const glob = require('glob')
+const sass = require('sass')
+const fiber = require('fibers')
+const Cleancss = require('clean-css')
+
+const postcssConfig = require('./postcss.config')
+
+const files = [
+  'bootstrap',
+  'bootstrap-grid',
+  'bootstrap-reboot',
+  'bootstrap-utilities'
+]
 
 files.forEach(filename => {
   sass.render({
     file: path.resolve(`./scss/${filename}.scss`),
+    fiber,
     outputStyle: 'expanded',
     sourceMap: true,
     sourceMapContents: true,
-    precision: 6,
     outFile: `./dist/css/${filename}.css`
   }, (error, css) => {
-    // Not sure what to do with css.map here
+    // Fix postcss to use and output sourcemaps
     postcss(postcssConfig)
       .process(css.css, { from: `./dist/css/${filename}.css` })
       .then(result => {
         fs.writeFile(`./dist/css/${filename}.css`, result.css, () => true)
-        fs.writeFile(`./dist/css/${filename}.min.css`, new cleancss().minify(result.css).styles, () => true)
+        fs.writeFile(`./dist/css/${filename}.min.css`, new Cleancss().minify(result.css).styles, () => true)
       })
   })
 })
 
-// Autoprefix examples
+// Prefix examples
 glob('./site/content/**/*.css', {}, (error, files) => {
   files.forEach(file => {
     fs.readFile(file, (err, css) => {
-
       postcss(postcssConfig)
         .process(css, { from: file, to: file })
         .then(result => {
